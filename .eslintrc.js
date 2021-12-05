f705ab725c4ea7aee051ed29f02e31170fb7f168
AhirSamaj <Ahirsamajvadodaraa@gmail.com>
2021-12-05 09:47:42 +0530

    basic vite boiler plate

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["plugin:vue/vue3-essential", "prettier"],
+  rules: {
+    // Override/add rules settings here, such as
+    "vue/no-unused-vars": "error",
+  },
+};
---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+    <h1 class="font-extrabold text-3xl mb-6">Home</h1>
+    <p class="mb-6">Store Name :- {{ name }}</p>
+    <input
+        type="text"
+        v-model="newName"
+        class="p-2 mr-4 border border-gray-600 rounded"
+        name="name"
+        id="txtName"
+        autocomplete="off"
+    />
+    <button @click="saveName" class="p-2 text-white bg-indigo-600 rounded">
+        Submit
+    </button>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const name = computed(() => {
+    return store.state.user.name
+})
+
+const newName = ref('')
+
+function saveName() {
+    store.dispatch('saveName', newName.value)
+    newName.value = ''
+    router.push('/about')
+}
+</script>
+
+<style scoped>
+a {
+    color: #42b983;
+}
+</style>
